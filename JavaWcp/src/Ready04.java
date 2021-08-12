96987c03836af7a2efbfee054d9a49920ed8bfa1
MiwaT <miwa951107@gmail.com>
2021-08-13 08:33:54 +0900

    Java curriculum8

---
+++ JavaWcp/src/Ready04.java
@@ -0,0 +1,17 @@
+
+public class Ready04 {
+	public static void main(String[] args) {
+		int number = 1;
+		printNum(number);
+	}
+	
+	public static int printNum(int number) {
+		if (number < 50) {
+			number *= 2;
+			System.out.println("A=" + number);
+			printNum(number);
+		}
+		return number;
+	}
+
+}
