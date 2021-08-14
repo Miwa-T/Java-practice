90b31ebd81af43890b838423de9421421de50f88
MiwaT <miwa951107@gmail.com>
2021-08-14 21:30:42 +0900

    curriculum9-1

---
+++ JavaWcp/src/ClassMethod01.java
@@ -0,0 +1,11 @@
+
+public class ClassMethod01 {
+	public static void main(String[] args) {
+        Ready05 yamada = new Ready05(); //Human01クラスのインスタンスを生成
+        System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+
+        Ready05 sato = new Ready05("佐藤", 25); //Human01クラスのインスタンスを生成
+        System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
+    }
+
+}
---
+++ JavaWcp/src/Ready05.java
@@ -0,0 +1,16 @@
+
+public class Ready05 {
+	public String name;
+	public int age;
+	
+	public Ready05() {
+		name = "山田";
+		age = 20;
+	}
+	
+	public Ready05(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
