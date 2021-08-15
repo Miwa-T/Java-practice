2556e077594097bc974e239f7e7ca8c8a47789e1
MiwaT <miwa951107@gmail.com>
2021-08-15 21:51:34 +0900

    test

--- JavaWcp/src/Ready04.java
+++ JavaWcp/src/Ready04.java
@@ -6,7 +6,7 @@ public class Ready04 {
 	}
 	
 	public static int printNum(int number) {
-		if (number < 90) {
+		if (number < 100) {
 			number *= 2;
 			System.out.println("A=" + number);
 			printNum(number);
