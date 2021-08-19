c62b8247e60835ec0ebb31b250cf88a28f8c7fa3
MiwaT <miwa951107@gmail.com>
2021-08-19 15:49:49 +0900

    curriculum 10

--- JavaWcp/src/Ready07.java
+++ JavaWcp/src/Ready07.java
@@ -1,4 +1,20 @@
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Ready07 {
+	public static void main(String[] args) {
+		try {
+			Ready07.readFile("exception.txt");
+		}catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException例外が発生");
+		}
+		System.out.println("プログラム終了");
+	}
+	public static void readFile(String fileName) throws FileNotFoundException {
+		System.out.println("ファイルの読み込み開始");
+		FileReader fr = new FileReader(fileName);
+		System.out.println(fileName + "の読み込み完了");
+	}
 
 }
