54e62d0f149779e2b58d7e88793a9d6050c3ae1d
MiwaT <miwa951107@gmail.com>
2021-08-20 17:32:47 +0900

    Java introduction chapter1-3

---
+++ JavaWcp/src/HelloWolrd2.java
@@ -0,0 +1,22 @@
+
+public class HelloWolrd2 {
+
+	public static void main(String[] args) {
+		/*
+		宣言(変数の型 変数名;)
+		※変数は宣言しただけでは空っぽ＝nullの状態なので
+		=を使い、右側の値を左側に代入する。
+		値の代入(変数=値;)
+		宣言したての変数は通常nullなので、この変数に初めて値を入力する事を初期化、代入された値を初期値という。
+		*/
+		String name = "佐藤";
+		int num = 551;
+		double dec = 67.8;
+		
+//		代入
+		System.out.println(name);
+		System.out.println(num);
+		System.out.println(dec);
+	}
+
+}
--- JavaWcp/src/Ready02.java
+++ JavaWcp/src/Ready02.java
@@ -1,7 +1,37 @@
-
 public class Ready02 {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		int num1 = 6;
+		int num2 = 4;
+		
+		int ans1 = num1 + num2;
+		int ans2 = num1 - num2;
+		int ans3 = num1 * num2;
+		double ans4 = (double) num1 / num2;
+		int ans5 = num1 % num2;
+		
+		System.out.println(ans1);
+		System.out.println(ans2);
+		System.out.println(ans3);
+		System.out.println(ans4);
+		System.out.println(ans5);
+		
+		int num3 = 346;
+		num3 = num3 + 4;
+		System.out.println("numの値" + num3);
+		
+		num3 += 5;
+//		num3 = num3 + 5と同じ意味
+		System.out.println("numの値" + num3);
+		
+		int num4 = 100;
+//		num4 = num4 + 1という意味
+		num4++;
+		System.out.println("numの値" + num4);
+		
+//		num4 = num4 - 1という意味
+		num4--;
+		System.out.println("numの値" + num4);
+		
 	}
 
 }
