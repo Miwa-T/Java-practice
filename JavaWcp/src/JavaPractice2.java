4abc86003c6c9be84f0212be10787f63ee09d387
MiwaT <miwa951107@gmail.com>
2021-08-30 10:33:41 +0900

    java introduction6 review

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,11 +2,10 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-		int[] nums = { 10, 20, 30};
-		for(int num: nums) {
+		int[] nums = {1, 2, 3};
+		for(int num : nums) {
 			System.out.println(num);
 		}
-		
 	}
 
 }
