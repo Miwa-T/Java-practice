46bec4839ba20e6877e491963e44362628b605ac
MiwaT <miwa951107@gmail.com>
2021-08-31 17:56:07 +0900

    Java review

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,13 +2,13 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-		int[] nums = {1, 2, 3};
-		for(int num : nums) {
-			System.out.println(num);
+		int i = 10;
+		
+		for(i = 10; i >= 0; i--) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
 		}
 	}
 
 }
-
-//※拡張for文: 配列の全ての値を単にループさせるには便利。
-//for(データ型 配列の各要素を格納する変数: 配列変数)
