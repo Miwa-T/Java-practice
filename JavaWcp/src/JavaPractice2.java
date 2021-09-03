66d4583ca3919a376c1b1403a3414a1e10cd3bfe
MiwaT <miwa951107@gmail.com>
2021-09-03 18:28:44 +0900

    Java introduction

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -3,17 +3,14 @@ public class JavaPractice2 {
 
 	public static void main(String[] args) {
 		int i = 1;
-		int count = 0;
-		int sum = 0;
+		int j = 3;
 		
-		for(i= 1; i <= 10; i++) {
-			if(i % 2 == 0) {
-				count++;
-				sum += i;
+		for(i = 1; i <= 3; i++) {
+			for(j = 3; j > 0; j--) {
+				if(i != j)
+				System.out.print("(" + i + "," + j + ")");
 			}
 		}
-		System.out.println("偶数の個数は" + count + "です");
-		System.out.println("偶数の合計は" + sum + "です");
 	}
 
 }
