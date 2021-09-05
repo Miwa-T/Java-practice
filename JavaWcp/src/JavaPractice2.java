7a3b1534c58fd33fd491b6716b14f75021486c06
MiwaT <miwa951107@gmail.com>
2021-09-05 17:15:05 +0900

    Java introduction

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,20 +2,7 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-        int dice = (int)(Math.random()*7)+1;
         
-        System.out.println("さいころの目:"+dice);
-        
-        if(1 <= dice && dice <= 6){
-            
-            if(dice == 2 || dice == 4 || dice == 6){
-                System.out.println("丁（チョウ）です。"); 
-            }else{
-                System.out.println("半（ハン）です。"); 
-            }
-        }else{
-            System.out.println("範囲外の数値です。");
-        }
     }
 
 }
