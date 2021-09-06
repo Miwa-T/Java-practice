7f39207495b7c08044aa0f8f1c422faf1668ebfb
MiwaT <miwa951107@gmail.com>
2021-09-06 19:54:37 +0900

    Java introduction

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,7 +2,12 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-        
+        int i = (int)(6 * Math.random()) + 1;
+        if(i != 6) {
+        	System.out.println("サイコロの目は" + i + "なのでサイコロが6になるまでもう一度振ります");
+        	i = (int)(6 * Math.random()) + 1;
+        }
+        System.out.println("サイコロの目が" + i + "になりました");
     }
 
 }
