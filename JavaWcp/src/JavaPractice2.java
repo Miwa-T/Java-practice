276f93d6fca7b32bef300c0ffd4488d35a262d0a
MiwaT <miwa951107@gmail.com>
2021-09-07 16:57:22 +0900

    Java introduction

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,12 +2,22 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-        int i = (int)(6 * Math.random()) + 1;
-        if(i != 6) {
-        	System.out.println("サイコロの目は" + i + "なのでサイコロが6になるまでもう一度振ります");
-        	i = (int)(6 * Math.random()) + 1;
-        }
-        System.out.println("サイコロの目が" + i + "になりました");
+        int[] list1 = { 1, 2, 3, 4, 5};
+        int[] list2 = { 6, 7, 8, 9, 10};
+        int[] list3 = { 11, 12, 13, 14, 15};
+        
+        showSumArray(list1);
+        showSumArray(list2);
+        showSumArray(list3);
+        
     }
+	
+	public static void showSumArray(int[] list) {
+		int sum = 0;
+		for(int a : list) {
+			sum += a;
+		}
+		System.out.println("配列内の合計:" + sum);
+	}
 
 }
