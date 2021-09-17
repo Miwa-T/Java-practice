08e66f7c9f586b1ea427bd2f76537110e38cb674
MiwaT <miwa951107@gmail.com>
2021-09-17 21:55:14 +0900

    Java introduction

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,10 +2,8 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-		int number = 1;
-        while (number < 50) {
-            number *= 2;
-            System.out.println("While01 = " + number);
+		for (int number = 1; number <= 5; number++) {
+            System.out.println("For01 = " + number);
         }
 	}
 
