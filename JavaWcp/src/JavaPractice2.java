239ac56172a12c24fff6600f12dc22b6218a3678
MiwaT <miwa951107@gmail.com>
2021-09-18 09:57:52 +0900

    Java introduction

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,8 +2,9 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-		for (int number = 1; number <= 5; number++) {
-            System.out.println("For01 = " + number);
+		int[] array = { 1, 2, 3, 4, 5 };
+        for (int number : array) {
+            System.out.println("For02 = " + number);
         }
 	}
 
