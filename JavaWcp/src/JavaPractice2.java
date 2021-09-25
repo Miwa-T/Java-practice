6cf16776a4b0911af102f4c8eb880992c5c42780
MiwaT <miwa951107@gmail.com>
2021-09-25 11:13:12 +0900

    test

--- JavaWcp/src/JavaPractice2.java
+++ JavaWcp/src/JavaPractice2.java
@@ -2,10 +2,15 @@
 public class JavaPractice2 {
 
 	public static void main(String[] args) {
-		int number = 1;
-        while (number < 50) {
-            number *= 2;
-            System.out.println("While01 = " + number);
+		int number = 7;
+        if (number < 5) {
+            System.out.println(number + " は5未満です");
+        } else if (number < 10) {
+            System.out.println(number + " は5以上、10未満です");
+        } else if (number < 15) {
+            System.out.println(number + " は10以上、15未満です");
+        } else {
+            System.out.println(number + " は15未満でないです");
         }
 	}
 
